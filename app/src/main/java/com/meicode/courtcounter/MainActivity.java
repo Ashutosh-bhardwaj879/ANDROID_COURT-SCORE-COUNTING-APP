b4d57a52523ce2e6aba984926b8ad3352fee896b
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2021-09-20 21:25:38 +0530

    class 40

--- app/src/main/java/com/meicode/courtcounter/MainActivity.java
+++ app/src/main/java/com/meicode/courtcounter/MainActivity.java
@@ -3,6 +3,7 @@ package com.meicode.courtcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,10 +15,47 @@ public class MainActivity extends AppCompatActivity {
         //displayForTeamA(8);
     }
 
+    int globalScoreA=0;
+    int globalScoreB=0;
+
     public void displayForTeamA(int score){
         TextView scoreView = (TextView)findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
+    public void addThreeForTeamA(View v){
+        globalScoreA+=3;
+        displayForTeamA(globalScoreA);
 
+    }
+
+    public void addTwoForTeamA(View v){
+        globalScoreA+=2;
+        displayForTeamA(globalScoreA);
+    }
+
+    public void addOneForTeamA(View v){
+        globalScoreA+=1;
+        displayForTeamA(globalScoreA);
+    }
+
+    public void displayForTeamB(int score){
+        TextView scoreView = (TextView)findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreeForTeamB(View v){
+        globalScoreB+=3;
+        displayForTeamB(globalScoreB);
+    }
+
+    public void addTwoForTeamB(View v){
+        globalScoreB+=2;
+        displayForTeamB(globalScoreB);
+    }
+
+    public void addOneForTeamB(View v){
+        globalScoreB+=1;
+        displayForTeamB(globalScoreB);
+    }
 }
